700558f7640b9ebe0a0bec274365414647cf1321
myieshac <myiesha.nvm@gmail.com>
2025-01-29 18:53:56 +0530

    Add files via upload

---
+++ CheckNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CheckNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = sc.nextInt();
+        
+        if (num > 0) {
+            System.out.println(num + " is a positive number.");
+        } else if (num < 0) {
+            System.out.println(num + " is a negative number.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+        
+        sc.close();
+    }
+}
