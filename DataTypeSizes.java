072c6d159fcec58bd43d0a1f50ac4cc416ca34a2
myieshac <myiesha.nvm@gmail.com>
2025-02-05 19:26:35 +0530

    Add files via upload

---
+++ DataTypeSizes.java
@@ -0,0 +1,12 @@
+public class DataTypeSizes {
+    public static void main(String[] args) {
+        System.out.println("Size of various data types in Java:");
+        System.out.println("Byte: " + Byte.SIZE / 8 + " bytes");
+        System.out.println("Short: " + Short.SIZE / 8 + " bytes");
+        System.out.println("Integer: " + Integer.SIZE / 8 + " bytes");
+        System.out.println("Long: " + Long.SIZE / 8 + " bytes");
+        System.out.println("Float: " + Float.SIZE / 8 + " bytes");
+        System.out.println("Double: " + Double.SIZE / 8 + " bytes");
+        System.out.println("Character: " + Character.SIZE / 8 + " bytes");
+    }
+}
