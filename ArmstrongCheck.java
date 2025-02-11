fab273e6957963195cc27adfe27ef90db23e1e8d
myieshac <myiesha.nvm@gmail.com>
2025-02-11 22:06:57 +0530

    Add files via upload

---
+++ ArmstrongCheck.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ArmstrongCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int original = num, sum = 0, digits = String.valueOf(num).length();
+        while (num != 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+        if (original == sum) {
+            System.out.println(original + " is an Armstrong number.");
+        } else {
+            System.out.println(original + " is not an Armstrong number.");
+        }
+        sc.close();
+    }
+}
---
+++ MaxMinTotal.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class MaxMinTotal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);  
+        System.out.println("Enter num 1: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter num 2: ");
+        int num2 = sc.nextInt();
+        System.out.println("Enter num 3: ");
+        int num3 = sc.nextInt();
+        int max = Math.max(num1, Math.max(num2, num3));
+        int min = Math.min(num1, Math.min(num2, num3));
+        int total = num1 + num2 + num3;
+        System.out.println("Maximum: " + max);
+        System.out.println("Minimum: " + min);
+        System.out.println("Total: " + total);
+        sc.close();
+    }
+}
