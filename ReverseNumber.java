b16b624406626fcae05b8223ad67700c20a3e955
myieshac <myiesha.nvm@gmail.com>
2025-02-13 18:41:32 +0530

    Add files via upload

---
+++ ReverseNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int num = sc.nextInt();
+        int reversed = 0;
+        while (num != 0) {
+            reversed = reversed * 10 + num % 10;
+            num /= 10;
+        }
+        System.out.println("Reversed number: " + reversed);
+        sc.close();
+    }
+}
